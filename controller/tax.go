466404438fdd7d7e2f02e604ea99e38614be33aa
wongsatorn.n <wongsatorn.n@hotmail.com>
2024-04-18 23:40:37 +0700

    create controller and tax calculation function

--- controller/tax.go
+++ controller/tax.go
@@ -2,19 +2,64 @@ package controller
 
 import (
 	"github.com/labstack/echo/v4"
+	model "github.com/son1122/assessment-tax/model"
 	"net/http"
 )
 
-// GetUsers handles the GET users route
-// @Summary Show account
-// @Description get string by ID
-// @ID get-string-by-int
+type taxLevelData struct {
+	Level string  `json:"level"`
+	Tax   float64 `json:"tax"`
+}
+
+func TaxCalculationFromTotalIncome(totalIncome float64) ([]taxLevelData, float64) {
+
+	totalIncome = 5000000
+	taxLevel, _ := model.GetTaxLevel()
+	var tax float64 = 0
+	var taxValueInLevel float64 = 0
+	//var sum float64 = 0
+	for i := 0; i < len(taxLevel); i++ {
+		if i == len(taxLevel)-1 {
+			taxValueInLevel = totalIncome - float64(taxLevel[i].Floor)
+			tax = tax + taxValueInLevel*float64(taxLevel[i].TaxValue)/100
+			break
+		}
+		if totalIncome >= float64(taxLevel[i].Ceil) {
+			if i > 0 {
+				tax = tax + (float64(taxLevel[i].TaxValue) * (float64(taxLevel[i].Ceil) - float64(taxLevel[i-1].Ceil)) / 100)
+			} else {
+				tax = tax + (float64(taxLevel[i].TaxValue) * (float64(taxLevel[i].Ceil)) / 100)
+			}
+
+		} else {
+			if totalIncome <= float64(taxLevel[i].Floor) {
+				break
+			}
+			taxValueInLevel = totalIncome - float64(taxLevel[i].Floor)
+			tax = tax + taxValueInLevel*float64(taxLevel[i].TaxValue)/100
+			break
+		}
+
+	}
+	//log.Printf(string(tax))
+	data := []taxLevelData{
+		taxLevelData{Level: "0-150,000", Tax: 0},
+		taxLevelData{Level: "0-150,001", Tax: 500},
+	}
+	return data, tax
+
+}
+
+// TaxCalculationPost handles the POST tax/calculations route
+// @Summary tax/calculations
+// @Description tax/calculations by data
 // @Accept  json
 // @Produce  json
-// @Param id path int true "Account ID"
 // @Success 200 {string} string "ok"
-// @Router /accounts/{id} [get]
-func TaxCalculation(c echo.Context) error {
-
-	return c.JSON(http.StatusOK, "tax calculation")
+// @Router /tax/calculations [post]
+func TaxCalculationPost(c echo.Context) error {
+	_, tax := TaxCalculationFromTotalIncome(123.55)
+	//data, _ := model.GetPersonalDeduct()
+	//data, _ := model.GetTaxLevel()
+	return c.JSON(http.StatusOK, tax)
 }
